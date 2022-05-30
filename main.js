0c65fd014cf7b0f6d2d33204f5f49624e7ed3845
carlsonnils <carlsonnils@gmail.com>
2022-05-29 22:58:42 -0600

    progress

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width = 200;
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.93);
 const car = new Car(
-    road.getLaneCenter(1),
+    road.getLaneCenter(1) + 10,
     100,
     30,
     50
@@ -15,7 +15,7 @@ animate();
 
 function animate() {
     car.update();
-    console.log(car.sensor)
+
     canvas.height = window.innerHeight;
 
     ctx.save();
--- sensor.js
+++ sensor.js
@@ -1,39 +1,36 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 3;
-        this.rayLength = 100;
-        this.raySpread = Math.PI / 4;
+        this.rayCount = 7;
+        this.rayLength = 150;
+        this.raySpread = Math.PI / 1.5;
 
         this.rays = [];
     }
 
     update() {
-        console.log('In sensor Update')
         this.rays = []
         for (let i = 0; i < this.rayCount; i++) {
             const rayAngle = lerp(
                 this.raySpread / 2, -this.raySpread / 2,
-                i / (this.rayCount - 1)
-            );
-            console.log(rayAngle)
+                this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+            ) + this.car.angle;
             const start = { x: this.car.x, y: this.car.y };
             const end = {
                 x: this.car.x - Math.sin(rayAngle) * this.rayLength,
                 y: this.car.y - Math.cos(rayAngle) * this.rayLength
             };
-            console.log(start, end)
 
             this.rays.push([start, end]);
         }
     }
 
     draw() {
+        this.update()
         for (let i = 0; i < this.rayCount; i++) {
             ctx.beginPath();
             ctx.lineWidth = 2;
             ctx.strokeStyle = "yellow";
-            // console.log(this.rays)
             ctx.moveTo(
                 this.rays[i][0].x,
                 this.rays[i][0].y
