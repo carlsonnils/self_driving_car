741b1e51c0a6c2efe1cfd46b1666c7abdae362df
carlsonnils <carlsonnils@gmail.com>
2022-05-29 20:10:31 -0600

    progress

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.93);
 const car = new Car(
     road.getLaneCenter(1),
-    window.innerHeight - 100,
+    100,
     30,
     50
 );
@@ -15,8 +15,15 @@ animate();
 
 function animate() {
     car.update();
+
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.8);
+
     road.draw(ctx)
     car.draw(ctx);
-    requestAnimationFrame(animate)
+
+    ctx.restore();
+    requestAnimationFrame(animate);
 }
